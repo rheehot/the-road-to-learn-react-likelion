5559b6cc1ecdce9260a18798523b6a63384511b4
Jongwoo-Han <han980817@gmail.com>
2020-01-04 07:55:46 +0900

    Modified index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,21 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 /* ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root")); */
+
+/*
+const tick = () => {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById("root"));
+};
+
+setInterval(tick, 1000);
+*/
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
