7a1c64df5c730b761d6331f728da7b02754f2f0f
Jongwoo-Han <han980817@gmail.com>
2020-01-04 09:04:03 +0900

    Modified index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-/* ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root")); */
+/* 
+const element = <h1>Hello, world!</h1>;
+ReactDOM.render(element, document.getElementById('root'));
+*/
 
 /*
 const tick = () => {
