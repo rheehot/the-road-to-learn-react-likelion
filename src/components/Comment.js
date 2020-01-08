2432d055e2f09e11aef88c8ae9778461580d7a76
Jongwoo-Han <han980817@gmail.com>
2020-01-08 10:13:29 +0900

    Added Components: Comment

---
+++ src/components/Comment.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Avatar from "./Avatar";
+
+/*
+const Comment = props => {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img
+          className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="UserInfo-name">{props.author.name}</div>
+      </div>
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">{props.date}</div>
+    </div>
+  );
+};
+*/
+
+const Comment = props => {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <Avatar user={props.author} />
+        <div className="UserInfo-name">{props.author.name}</div>
+      </div>
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">{props.date}</div>
+    </div>
+  );
+};
+
+export default Comment;
